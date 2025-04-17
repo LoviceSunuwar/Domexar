652bf07b78423a8c2ed87fe9b8ed63de12ef5cbb
Lovice Sunuwar <Lovicesunuwar@gmail.com>
2025-04-16 21:03:38 -0400

    Demo V1 Completed.

---
+++ Domexar/Views/WalletConnectView.swift
@@ -0,0 +1,18 @@
+//
+//  WalletConnectView.swift
+//  Domexar
+//
+//  Created by Lovice Sunuwar on 17/03/2025.
+//
+
+import SwiftUI
+
+struct WalletConnectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WalletConnectView()
+}
